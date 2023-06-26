754d9450df48ff522536dc6b90dcf53a26d99218
sysadminrus <sysadminrus@gmail.com>
2023-06-26 17:36:24 +0500

    Доработано открытие, закрытие смены

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ from kktfunc.cashier import Cashier #Кассира из json в объект
 
 app = Flask(__name__)
 
+
+def returnedjson(result:dict):
+    json.dumps(result)
+
 @app.route("/")
 def hello():
     return "Hello, World!"
 
 @app.route("/check", methods=['POST'])
 def checkstatus():
-    kkt = kkt.initkkt(None)
+    kkt = kkt.initK(None)
     if kkt.isinstance(IFptr):
         pass
     else: #Возвращаем ошибку инциализации
@@ -21,23 +25,46 @@ def checkstatus():
         
 
 @app.route("/openShift", methods=['POST'])
-def openShift(cashier):
-    kkt = kkt.initkkt(None)
-    if kkt.isinstance(IFptr):
-        if kkt.isOpened():
-            return "error shift opened"
+def openShift():
+    inpjson = request.json
+    initedkkt = kkt.initKKT(None)
+    if initedkkt.get('succes'):
+        driver = initedkkt.get('driver')
+        # if driver.LIBFPTR_SS_EXPIRED > 0:
+        #     driver.close()
+        #     return json.dumps({'error': 'Смена открыта более 24 часов',
+        #                        'success': False},ensure_ascii=False)
+        # if driver.isOpened():
+        #     driver.close()
+        #     return json.dumps({'error': 'Смена уже открыта',
+        #                        'success': True},ensure_ascii=False)
+        shiftresult = kkt.openShift(inpjson, driver)
+        if shiftresult.get('succes'):
+            return json.dumps({'succes':True,
+                               'error':''},ensure_ascii=False)
         else:
-            pass
-    else: #Возвращаем ошибку инциализации
-        return kkt
+            errorstring = f'Ошибка при открытии смены {shiftresult.get("descr")}'
+            return json.dumps({'succes': True, 'error': errorstring}, ensure_ascii=False)
+    
 
 @app.route("/closeShift", methods=['POST'])
-def closeShift(cashier):
-    kkt = kkt.initkkt(None)
-    if kkt.isinstance(IFptr):
-        pass
-    else: #Возвращаем ошибку инциализации
-        return kkt
+def closeShift():
+    inpjson = request.json
+    initedkkt = kkt.initKKT(None)
+    if initedkkt.get('succes'):
+        driver = initedkkt.get('driver')
+        if driver.isOpened():
+            result = kkt.closeShift(inpjson, driver)
+            if result.get('succes'):
+                driver.close()
+                return json.dumps({'succes': True,
+                                   'error':''})
+            else:
+                driver.close()
+                return json.dumps({'succes': False, 'error':result.get('descr')},ensure_ascii=False)
+        else:
+            driver.close()
+            return json.dumps({'succes': False, 'error':'Смена закрыта'}, ensure_ascii=False)
 
 @app.route("/receipt", methods=['POST'])
 def receipt(receiptData):
@@ -49,5 +76,5 @@ def receipt(receiptData):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
     
--- kktfunc/kktfunctions.py
+++ kktfunc/kktfunctions.py
@@ -1,10 +1,10 @@
 import json
 from logging import raiseExceptions
-from kktfunc.initkkt import initkkt
+#from kktfunc.initkkt import initkkt
 from libfptr10 import IFptr
 
-def returnDict(success: bool, errorDesc: str):
-    return {'succes':success, 'descr':errorDesc}
+def returnDict(success: bool, errorDesc: str, fptr: IFptr):
+    return {'succes':success, 'descr':errorDesc, 'driver': fptr}
 
 #Проверка кода маркировки
 def checkdm(fptr):
@@ -36,9 +36,9 @@ def initKKT(settings: dict[str, any]):
     fptr.setSettings(settings)
     fptr.open()
     if fptr.isOpened():
-        return returnDict(True,'')
+        return returnDict(True,'', fptr)
     else:
-        return returnDict(False, fptr.errorDescription())
+        return returnDict(False, fptr.errorDescription(), None)
 
 # Установка кассира
 def setcashier(cashier: json, fptr: IFptr):
@@ -46,32 +46,50 @@ def setcashier(cashier: json, fptr: IFptr):
     fptr.setParam(1203, "123456789047")
     fptr.operatorLogin()
     if fptr.errorCode() == 0:
-        return returnDict(True, '')
+        return returnDict(True, '', None)
     else:    
-        return returnDict(False, fptr.errorDescription())
+        return returnDict(False, fptr.errorDescription(), None)
 
 #Открытие смены
-def openShift(cashier: json):
+def openShift(cashier: json, fptr: IFptr):
     if cashier:
-        pass
+        setcashier(cashier, fptr)
     else: #TODO кассир не пришел, надо взять из БД
-        pass
-    fptr = initKKT(None)
-    if fptr.isinstance(IFptr):
-        fptr.openShift()
-        if fptr.isOpened():
-            return returnDict(True, '')
-        else:
-            return returnDict(False, fptr.errorDescription())
+        setcashier(cashier, fptr)
+    #fptr = initKKT(None)
+    #if fptr.isinstance(IFptr):
+    fptr.openShift()
+    if fptr.errorCode() == 0:
+        return returnDict(True, '', None)
     else:
-        return fptr.errorDescription()
-
+        return returnDict(False, fptr.errorDescription(), None)
+    
 #Закрытие смены
-def closeShift(cashier: json):
+def closeShift(cashier: json, fptr: IFptr):
     if cashier:
-        pass
+        setcashier(cashier, fptr)
+    else: #TODO кассир не пришел, надо взять из БД
+        setcashier(cashier, fptr)
+    #fptr = initKKT(None)
+    #if fptr.isinstance(IFptr):
+    if fptr.isOpened():
+        fptr.setParam(IFptr.LIBFPTR_PARAM_REPORT_TYPE, IFptr.LIBFPTR_RT_CLOSE_SHIFT)
+        fptr.report()
+        if fptr.errorCode == 0:
+            return returnDict(True, '', None)
+        else:
+            return returnDict(False, fptr.errorDescription(), None)
     else:
-        pass
-    fptr = initKKT(None)
-    
-    
+        return returnDict(False, fptr.errorDescription())
+
+#Получить настройки ККТ
+def getkktsettings():
+    settings = {
+    IFptr.LIBFPTR_SETTING_MODEL: IFptr.LIBFPTR_MODEL_ATOL_11F,
+    IFptr.LIBFPTR_SETTING_PORT: IFptr.LIBFPTR_PORT_COM,
+    IFptr.LIBFPTR_SETTING_COM_FILE: "COM5",
+    IFptr.LIBFPTR_SETTING_BAUDRATE: IFptr.LIBFPTR_PORT_BR_115200}
+    return settings
+
+if __name__ == '__main__':
+    print('Not for start')

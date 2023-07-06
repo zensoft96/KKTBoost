25b23f5e067c1f7e812fbcf8c82db7389187ab29
Mhch497 <mhch41997@gmail.com>
2023-07-06 11:53:28 +0500

    изменение свойства, и формы настроек

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def checkmark():
 
 @app.route("/settings", methods=['POST','GET'])
 def settings():
+    settingsdict = {}
     if request.method == 'POST':
         for i in request.form:
             current_settings = Settings.get_or_none(Settings.setting == i)
@@ -63,7 +64,13 @@ def settings():
                 current_settings.setting = i
                 current_settings.settingvalue = request.form[i]
                 current_settings.save()
-        return render_template('settings.html', disabled = 'disabled')
+            
+            settingsdict[current_settings.setting] = current_settings.settingvalue
+        return render_template('settings.html', disabled = 'disabled', 
+                               model=settingsdict.get('LIBFPTR_SETTING_MODEL'),
+                                   port = settingsdict.get('LIBFPTR_SETTING_PORT'), 
+                                   com = settingsdict.get('LIBFPTR_SETTING_COM_FILE'), 
+                                   baud = settingsdict.get('LIBFPTR_SETTING_BAUDRATE'))
     elif request.method == 'GET':
         current_settings = Settings.select().dicts()
         if len(current_settings) > 0:
@@ -91,10 +98,10 @@ def checkstatus():
     if request.content_type == 'application/x-www-form-urlencoded':
         settings = kkt.setkktsettingsfromform(request.form)
         initedkkt = kkt.initKKT(settings)
-        return render_template('settings.html', model=settings.get('Model'),
-                                   port = settings.get('Port'), 
-                                   com = settings.get('ComFile'), 
-                                   baud = settings.get('BaudRate'), tested = initedkkt.get('succes'), error = initedkkt.get('descr'))
+        return render_template('settings.html', model=settings.get('LIBFPTR_SETTING_MODEL'),
+                                   port = settings.get('LIBFPTR_SETTING_PORT'), 
+                                   com = settings.get('LIBFPTR_SETTING_COM_FILE'), 
+                                   baud = settings.get('LIBFPTR_SETTING_BAUDRATE'), tested = initedkkt.get('succes'), error = initedkkt.get('descr'))
         
 @app.route("/openShift", methods=['POST'])
 def openShift():
